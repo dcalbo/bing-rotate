b81ceae6510bd1554c52139c80150eb568a5119d
Diego G. Calbo Elizondo <dgcalbo@gmail.com>
2020-02-23 12:44:47 -0300

    Readme and bing-rotate files completed.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# bing-rotate
+# Little script to set wallpaper dynamically
+
 Set wallpaper on desktop Gnome 3 and Cinnamon using the 'Picture of the day' from Bing, a.k.a. Windows 10.
+
+## $xmlURL is needed to get the xml data from which the relative URL for the Bing pic of the day is extracted.
+
+The format parameter determines what language gets the information. 
+Valid values are: xml (XML), RSS (rss) and JSON (js).
+
+The idx parameter determines where to start from. 0 is the current day, 1 the previous day, etc.
+
+The mkt parameter determines which Bing market you would like to obtain your images from.
+Valid values are: en-US, es-ES, zh-CN, ja-JP, en-AU, en-UK, de-DE, en-NZ, en-CA.
+
+xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-EN"
+
+## The desired Bing picture resolution to download
+
+Valid options: "_1024x768" "_1280x720" "_1366x768" "_1920x1080" "_1920x1200"
+If you wont Bing's watermark don't use 1920x1200.
+
+## Check for a new image every hour
+
+If you want to check for a new image every hour, you can configure crontab.
+
+0 * * * * ~/path-to/bing-rotate.sh
+
+Enjoy!!!
---
+++ bing-rotate.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# $bing is needed to form the fully qualified URL for
+# the Bing pic of the day
+bing="http://www.bing.com"
+
+# $xmlURL is needed to get the xml data from which
+# the relative URL for the Bing pic of the day is extracted
+# The idx parameter determines where to start from. 0 is the current day,
+# 1 the previous day, etc.
+xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-EN"
+
+# $saveDir is used to set the location where Bing pics of the day
+# are stored. $HOME holds the path of the current user's home directory
+saveDir=$HOME'/Pictures/Bing/'
+
+# Create saveDir if it does not already exist
+mkdir -p $saveDir
+
+# Set picture options
+# Valid options are: none,wallpaper,centered,scaled,stretched,zoom,span ned
+picOpts="zoom"
+
+# The desired Bing picture resolution to download
+# Valid options: "_1024x768" "_1280x720" "_1366x768" "_1920x1200"
+picRes="_1920x1080"
+
+# The file extension for the Bing pic
+picExt=".jpg"
+
+# Extract the relative URL of the Bing pic of the day from
+# the XML data retrieved from xmlURL, form the fully qualified
+# URL for the pic of the day, and store it in $picURL
+picURL=$bing$(echo $(curl -s $xmlURL) | grep -oP "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)$picRes$picExt
+
+# Extract the description of the Bing pic of the day from
+# the XML data retrieved from xmlURL, form the fully qualified
+# URL for the pic of the day, and store it in $picDesc
+picDesc=$(echo $(curl -s $xmlURL) | grep -oP "<copyright>(.*)</copyright>" | cut -d ">" -f 2 | cut -d "(" -f 1)
+
+# Extract the author of the Bing pic of the day from
+# the XML data retrieved from xmlURL, form the fully qualified
+# URL for the pic of the day, and store it in $picAuth
+picAuth=$(echo $(curl -s $xmlURL) | grep -oP "<copyright>(.*)</copyright>" | cut -d "(" -f 2 | cut -d ")" -f 1)
+
+
+# Temporary Name
+tempName="temp.jpg"
+
+# Download the Bing pic of the day
+curl -s -o $saveDir$tempName $picURL
+
+# Get the description from the EXIF of the image
+picExif=$(exiftool -s -s -s -Title "$saveDir$tempName")
+
+if [ $picRes == "_1920x1200" ]
+    then annotation=$picExif
+else
+      annotation="$picDesc\nAuthor: $picAuth" 
+fi
+
+# Final Name given by Bing
+picName=${picURL##*/} 
+picFileName=${picName%.*}.png
+
+# Dir to place the altered images (with image description)
+deskDir="${saveDir}desktop/"
+
+# Make sure the directory exists
+mkdir -p $deskDir
+
+# Write the EXIF into the Wallpaper
+#convert $saveDir$tempName  -fill white  -undercolor '#00000080' -font Ubuntu-Medium -pointsize 24 -gravity Southeast -annotate +0+5 " $annotation " $deskDir$picFileName
+# Ubuntu Unity (no bottom menu)
+#convert $saveDir$tempName  -fill white  -undercolor '#00000080' -font Ubuntu-Medium -pointsize 24 -gravity Southeast -annotate +0+61 " $annotation " $deskDir$picFileName
+# Cinnamon (bottom menu)
+convert $saveDir$tempName  -fill white  -undercolor '#00000080' -font Ubuntu-Medium -pointsize 24 -gravity Southeast -annotate +0+86 " $annotation " $deskDir$picFileName
+
+# Remove Temp
+#rm $saveDir$tempName
+mv $saveDir$tempName $saveDir$picName
+
+# Set the GNOME3 wallpaper
+#DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background picture-uri '"file://'$deskDir$picFileName'"'
+
+# Set the GNOME 3 wallpaper picture options
+#DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background picture-options $picOpts
+
+# Set the Cinnamon wallpaper
+DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.cinnamon.desktop.background picture-uri '"file://'$deskDir$picFileName'"'
+
+# Remove pictures older than 7 days
+find $deskDir -atime 14 -delete
+
+# Exit the script
+exit
